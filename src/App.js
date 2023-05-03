85b99220b542c8baffca99527ce58f65eb455897
StonesCutter <martino.manzolini@beije.it>
2023-05-03 12:25:42 +0300

    First commit
    Uploading folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/functionalComponents/button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
